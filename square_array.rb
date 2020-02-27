20ab1b06e01e3572ba5a96b168cc11d2d6b20c47
interpreted-keyword-9673 <mel.cantorias@gmail.com>
2020-02-27 20:26:29 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -1,7 +1,6 @@
 def square_array(array)
-  new_array = []
-  array.each {|x| new_array << x**2}
-  return new_array
+  array.each {|x| x**2}
+  return array
 end
 numbers = [1,2,3]
 new_numbers = [9,10,16,25]
