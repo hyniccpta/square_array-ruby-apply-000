d84f38381ed152ea7e47b5247af1b378dcff43b5
interpreted-keyword-9673 <mel.cantorias@gmail.com>
2020-02-27 20:43:00 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -4,5 +4,5 @@ def square_array(array)
 end
 numbers = [1,2,3]
 new_numbers = [9,10,16,25]
-square_array(numbers)
-square_array(new_numbers)
+print square_array(numbers)
+print square_array(new_numbers)
